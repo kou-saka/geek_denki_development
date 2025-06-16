956c2e53d6535138db3aa40add92a107f2299644
kou-saka <juntakasaka5240@gmail.com>
2025-06-16 15:36:23 +0900

    Initial Spring Boot + Thymeleaf skeleton

---
+++ src/main/java/com/example/geek_denki_development/GeekDenkiDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.geek_denki_development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekDenkiDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekDenkiDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/geek_denki_development/GeekDenkiDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geek_denki_development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekDenkiDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
